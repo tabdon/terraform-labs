d21d1e7547c812b863f409c1b1a0d769d47ca658
Tobias Abdon <tabdon@gmail.com>
2021-07-06 06:24:35 +0700

    Create lab_challenges.tf

---
+++ module01/lab_challenges.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.42"
+    }
+  }
+  required_version = ">= 0.15.3"
+}
+
+provider "aws" {
+  profile = "skillmix-lab"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "smx123yzap"
+  acl = "private"
+
+}
+
+resource "aws_iam_group" "admins" {
+  name = "admins"
+}
+
+resource "aws_iam_user" "boss" {
+  name = "Superman"
+}
+
+resource "aws_iam_user_group_membership" "add_boss" {
+  user = aws_iam_user.boss.name
+
+  groups = [
+    aws_iam_group.admins.name
+  ]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical's AWS Account
+}
+
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+}
