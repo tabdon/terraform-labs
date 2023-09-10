8a456e453c5d1f6afbc62dd19e4eac5f2cb61e7c
Tobias Abdon <tabdon@gmail.com>
2023-09-10 11:27:32 +0700

    Update lab_challenges.tf

--- module01/lab_challenges.tf
+++ module01/lab_challenges.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.42"
+      version = "~> 5.16"
     }
   }
   required_version = ">= 0.15.3"
@@ -13,13 +13,8 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket_acl" "my_bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "smx123yzap"
+  bucket = "<your_bucket_name>"
 }
 
 resource "aws_iam_group" "admins" {
